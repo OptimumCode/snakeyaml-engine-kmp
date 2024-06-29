b6d87dd21d221cf44449280912dd9df4c2599740
github-action-benchmark <github@users.noreply.github.com>
2024-06-29 17:02:51 +0000

    add SnakeKMP benchmark (jmh) benchmark result for 8cc0f655890457cd4db7c5c173705c1db6a7439b

---
+++ dev/bench/data.js
@@ -0,0 +1,38 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1719680571037,
+  "repoUrl": "https://github.com/OptimumCode/snakeyaml-engine-kmp",
+  "entries": {
+    "SnakeKMP benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "oleg.smirnov@exactprosystems.com",
+            "name": "Oleg",
+            "username": "OptimumCode"
+          },
+          "committer": {
+            "email": "oleg.smirnov@exactprosystems.com",
+            "name": "Oleg",
+            "username": "OptimumCode"
+          },
+          "distinct": true,
+          "id": "8cc0f655890457cd4db7c5c173705c1db6a7439b",
+          "message": "Add comment and alerting",
+          "timestamp": "2024-06-29T21:00:45+04:00",
+          "tree_id": "d37759d0740563c7249ba66869d63cd450e6b90b",
+          "url": "https://github.com/OptimumCode/snakeyaml-engine-kmp/commit/8cc0f655890457cd4db7c5c173705c1db6a7439b"
+        },
+        "date": 1719680570602,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "it.krzeminski.snakeyaml.engine.kmp.benchmark.LoadingTimeBenchmark.loadsOpenAiSchema ( {\"openAiYamlPath\":\"/home/runner/work/snakeyaml-engine-kmp/snakeyaml-engine-kmp/benchmark/data/issues/kmp-issue-204-OpenAI-API.yaml\"} )",
+            "value": 21.28464953242564,
+            "unit": "ms/op",
+            "extra": "iterations: 10\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ]
+  }
+}
