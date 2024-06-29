96e281302b3003238a33c5ce5595314b826e42e7
github-action-benchmark <github@users.noreply.github.com>
2024-06-29 16:56:24 +0000

    add SnakeKMP benchmark (jmh) benchmark result for 90c588e7ca4ce924021d868f62d048de1bf81b58

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719679925278,
+  "lastUpdate": 1719680184450,
   "repoUrl": "https://github.com/OptimumCode/snakeyaml-engine-kmp",
   "entries": {
     "My Project Go Benchmark": [
@@ -28,6 +28,38 @@ window.BENCHMARK_DATA = {
           {
             "name": "it.krzeminski.snakeyaml.engine.kmp.benchmark.LoadingTimeBenchmark.loadsOpenAiSchema ( {\"openAiYamlPath\":\"/home/runner/work/snakeyaml-engine-kmp/snakeyaml-engine-kmp/benchmark/data/issues/kmp-issue-204-OpenAI-API.yaml\"} )",
             "value": 20.315460919112265,
+            "unit": "ms/op",
+            "extra": "iterations: 10\nforks: 1\nthreads: 1"
+          }
+        ]
+      }
+    ],
+    "SnakeKMP benchmark": [
+      {
+        "commit": {
+          "author": {
+            "email": "oleg.smirnov@exactprosystems.com",
+            "name": "Oleg",
+            "username": "OptimumCode"
+          },
+          "committer": {
+            "email": "oleg.smirnov@exactprosystems.com",
+            "name": "Oleg",
+            "username": "OptimumCode"
+          },
+          "distinct": true,
+          "id": "90c588e7ca4ce924021d868f62d048de1bf81b58",
+          "message": "Rename benchmark",
+          "timestamp": "2024-06-29T20:53:52+04:00",
+          "tree_id": "a2023929e672d329348ac2999a9d0abec6599a44",
+          "url": "https://github.com/OptimumCode/snakeyaml-engine-kmp/commit/90c588e7ca4ce924021d868f62d048de1bf81b58"
+        },
+        "date": 1719680183496,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "it.krzeminski.snakeyaml.engine.kmp.benchmark.LoadingTimeBenchmark.loadsOpenAiSchema ( {\"openAiYamlPath\":\"/home/runner/work/snakeyaml-engine-kmp/snakeyaml-engine-kmp/benchmark/data/issues/kmp-issue-204-OpenAI-API.yaml\"} )",
+            "value": 20.355723659708023,
             "unit": "ms/op",
             "extra": "iterations: 10\nforks: 1\nthreads: 1"
           }
