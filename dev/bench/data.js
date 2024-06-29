78230604946eab3daee2adf52611f4a6c2125354
github-action-benchmark <github@users.noreply.github.com>
2024-06-29 17:11:02 +0000

    add SnakeKMP benchmark (jmh) benchmark result for e47bbc90fba44867ef54533701d377e3ae612660

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1719680571037,
+  "lastUpdate": 1719681062277,
   "repoUrl": "https://github.com/OptimumCode/snakeyaml-engine-kmp",
   "entries": {
     "SnakeKMP benchmark": [
@@ -28,6 +28,36 @@ window.BENCHMARK_DATA = {
           {
             "name": "it.krzeminski.snakeyaml.engine.kmp.benchmark.LoadingTimeBenchmark.loadsOpenAiSchema ( {\"openAiYamlPath\":\"/home/runner/work/snakeyaml-engine-kmp/snakeyaml-engine-kmp/benchmark/data/issues/kmp-issue-204-OpenAI-API.yaml\"} )",
             "value": 21.28464953242564,
+            "unit": "ms/op",
+            "extra": "iterations: 10\nforks: 1\nthreads: 1"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "oleg.smirnov@exactprosystems.com",
+            "name": "Oleg",
+            "username": "OptimumCode"
+          },
+          "committer": {
+            "email": "oleg.smirnov@exactprosystems.com",
+            "name": "Oleg",
+            "username": "OptimumCode"
+          },
+          "distinct": true,
+          "id": "e47bbc90fba44867ef54533701d377e3ae612660",
+          "message": "Add pull_request as trigger",
+          "timestamp": "2024-06-29T21:09:01+04:00",
+          "tree_id": "f27c5f3f6c6949a1316c4f4650f66f66c3369567",
+          "url": "https://github.com/OptimumCode/snakeyaml-engine-kmp/commit/e47bbc90fba44867ef54533701d377e3ae612660"
+        },
+        "date": 1719681061851,
+        "tool": "jmh",
+        "benches": [
+          {
+            "name": "it.krzeminski.snakeyaml.engine.kmp.benchmark.LoadingTimeBenchmark.loadsOpenAiSchema ( {\"openAiYamlPath\":\"/home/runner/work/snakeyaml-engine-kmp/snakeyaml-engine-kmp/benchmark/data/issues/kmp-issue-204-OpenAI-API.yaml\"} )",
+            "value": 18.845666309762898,
             "unit": "ms/op",
             "extra": "iterations: 10\nforks: 1\nthreads: 1"
           }
